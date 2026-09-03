dfb6aa2e68f32b99c9fcbc47108826d7c6d7bca0
hanju <rlagkswn97@gmail.com>
2026-09-03 11:50:40 +0900

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from sqlalchemy import create_engine , Table , Column, Integer, String, MetaData
+import pandas as pd
+from faker import Faker
+
+engine = create_engine('sqlite:///mydatabase.db')
+metadata = MetaData()
+
+users_table = Table('users' , metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('email', String),
+    Column('address' , String))
+
+metadata.create_all(engine)
+
+fake = Faker()
+
+def generate_fake_data(n=10) :
+    with engine.connect() as conn:
+        conn.execute(users_table.delete())
+        for _ in range(n):
+            conn.execute(users_table.insert().values(
+                name=fake.name(),
+                email=fake.email(),
+                address=fake.address()
+            ))
+        conn.commit()
+
+if st.button('Generate Fake Data'):
+    generate_fake_data(20)
+    st.success('Fake data generated generated!')
+
+@st.cache_data
+def load_data() :
+    with engine.connect() as conn:
+        query = 'SELECT * FROM users'
+        return pd.read_sql(query, conn)
+
+data = load_data()
+st.write(data)
